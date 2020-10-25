e3b79842a37536d83ec84e561a45f9e27916855b
satyamchouksey <satyamchouksey09@gmail.com>
2020-10-25 18:44:42 +0530

    state change effect on render (in simple function and fetch calls)

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
 import React, { Component } from "react";
-import { Button } from 'reactstrap';
+import { Button } from "reactstrap";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      do: true
+      do: true,
+      hi:false
     };
-    this.setRSelected = this.setRSelected.bind(this)
+    this.setRSelected = this.setRSelected.bind(this);
     // this.fetcing = this.fetcing.bind(this)
- }
-  shouldComponentUpdate(nextProps, nextState){
-    if(this.state.do === nextState.do ){
-      return false
-    }
-    return true
   }
-  fetching = () =>{
-    console.log("fetching started")
-    return fetch('https://randomuser.me/api/?results=10')
-  .then(response => response.json())
-  .then(data => console.log(data));
-  
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (this.state.do === nextState.do) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
+  fetching = () => {
+    console.log("fetching started");
+    fetch("https://randomuser.me/api/?results=10")
+      .then(response => response.json())
+      .then(data =>{ console.log(data);this.setState({ hi: true });console.log("DONE");this.setState({ do: true })});
+  };
   componentDidMount() {
-    console.log("renderBefore")
+    console.log("renderBefore");
     this.setState({ do: true });
-    console.log("renderAfter")
+    console.log("renderAfter");
   }
-  setRSelected(){
+  setRSelected() {
+    console.log("button clicked");
     this.setState({ do: "HEllo" });
-    const fetched = this.fetching();
-    console.log("fetching done")
-    return fetched;
-    
+    this.fetching();
+
+    console.log("fetching over");
+    this.setState({ do: false });
+    console.log("1");
+    this.setState({ hi: false })
+    console.log("fetching done");
   }
   render() {
-    console.log("Rendering")
+    console.log("Rendering");
     return (
       <div>
-        <Button color="primary" onClick={() => this.setRSelected(1)} >One</Button>
-      {/* <div>{console.log()}</div> */}
-      <h1>Hi</h1>
+        <Button color="primary" onClick={() => this.setRSelected(1)}>
+          One
+        </Button>
+        {/* <div>{console.log()}</div> */}
+        <h1>Hi</h1>
       </div>
-    )
+    );
   }
 }
 
